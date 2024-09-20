2254fe37c1e1be47c3dd620acd29aea33bac7de2
AnixGG <g4zizullinn@yandex.ru>
2024-09-20 23:47:54 +0300

    Initial Commit

---
+++ ios app works/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios app works
+//
+//  Created by Нияз Газизуллин on 20.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
